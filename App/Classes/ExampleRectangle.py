75213be7d6f565da97483b8d4725d666e6519a87
zannah-rain <zoe.pellatt.0@gmail.com>
2019-05-13 01:26:00 +0100

    Move all example rectangle logic into its class

--- App/Classes/ExampleRectangle.py
+++ App/Classes/ExampleRectangle.py
@@ -10,6 +10,18 @@ class ExampleRectangle:
         self.width = width
         self.height = height
         self.colour = (255, 0, 0)
+        self.speed = 5
 
     def draw(self, window):
         pygame.draw.rect(window, self.colour, (self.x, self.y, self.width, self.height))
+
+    def step(self, keys):
+        # respond to key presses
+        if keys[pygame.K_LEFT]:
+            self.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.x += self.speed
+        if keys[pygame.K_UP]:
+            self.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.y += self.speed
--- App/Game.py
+++ App/Game.py
@@ -17,13 +17,6 @@ HEAT_SLOWNESS = 50
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('BrainChild')
 
-# Initial example rectangle
-x = 50
-y = 50
-width = 40
-height = 60
-speed = 5
-
 # Instantiate an ExampleRectangle
 exampleRectangle = ExampleRectangle(100, 100, 20, 20)
 
@@ -38,30 +31,19 @@ temperature_surface.set_alpha(64)  # Up to 128
 running = True
 while running:
 
-    # Initial frame rate limit
-    # pygame.time.delay(100)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
 
-    # Move the rectangle about
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    if keys[pygame.K_RIGHT]:
-        x += speed
-    if keys[pygame.K_UP]:
-        y -= speed
-    if keys[pygame.K_DOWN]:
-        y += speed
+    # Update the example rectangle
+    exampleRectangle.step(keys)
 
     # Fills the window with the background colour
     window.fill(BACKGROUND_COLOUR)
 
     # Draws the example rectangle
-    pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
     exampleRectangle.draw(window)
 
     # Draw heatmap
